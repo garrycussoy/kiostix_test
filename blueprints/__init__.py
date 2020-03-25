42d75a24c009c2e2ae734414c855540f47a21850
garrycussoy <garryarielcussoy@gmail.com>
2020-03-25 10:13:05 +0700

    Inisialization

---
+++ blueprints/__init__.py
@@ -0,0 +1,62 @@
+# Import from standard libraries
+import json
+import os
+from datetime import timedelta
+from functools import wraps
+
+# Import from related third party
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+from flask_cors import CORS
+
+# A step to prevent CORS
+app = Flask(__name__)
+CORS(app)
+
+# Set the following to true, if you want to auto-reload when there is a change, or false otherwise
+app.config['APP_DEBUG'] = True
+
+# ---------- Database Setup ----------
+# Connect to database
+try:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Garryac1@localhost:3306/kiostix_test'
+except Exception as e:
+    raise e
+
+# Setup database migration
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+# ---------- End of database setup ----------
+
+# Import modules related to routing
+# from blueprints.auth.__init__ import bp_auth
+
+# Register routes
+# app.register_blueprint(bp_auth, url_prefix='/login-admin')
+
+# Create the database
+db.create_all()
+
+# Handle response from a request
+@app.after_request
+def after_request(response):
+    try:
+        request_data = response.get_json()
+    except Exception as e:
+        request_data = response.args.to_dict()
+    
+    log_data = json.dumps({
+        'status_code': response.status_code,
+        'method': request.method,
+        'code': response.status,
+        'uri': request.full_path,
+        'requedatetimest': request_data,
+        'response': json.loads(response.data.decode('utf-8'))
+    })
+    log = app.logger.info("REQUEST_LOG\t%s", log_data) if response.status_code == 200 else app.logger.warning("REQUEST_LOG\t%s", log_data)
+    return response
